51f0e78f319fc846a532cbde8edff36153af6ef7
Marmiz <claudio.restifo@gmail.com>
2021-04-22 18:51:37 +0200

    removed unused resources

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,4 +1,3 @@
-import Avatar from "./avatar";
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import PostTitle from "./post-title";
@@ -19,9 +18,6 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
-        <div className="block hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
